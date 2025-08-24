80adae37f57cad8179317d1e791d49ee4eeaf65e
Alish Gautam <alishgautam12@gmail.com>
2025-08-24 10:51:45 +0545

    update

--- src/components/store/useItemStore.ts
+++ src/components/store/useItemStore.ts
@@ -1,35 +1,3 @@
-// import { create } from "zustand";
-
-// export interface Item {
-//   id: number;
-//   itemCode: string;
-//   itemName: string;
-//   category: string;
-//   quantity: number;
-//   uom: string;
-//   price: number;
-//   tax: number;
-//   supplier: string;
-//   reorderLevel: number;
-//   minOrderQty: number;
-//   preferredStock: number;
-//   expiryDate: string;
-//   customAttributes: string;
-//   description: string;
-// }
-
-// interface ItemStore {
-//   items: Item[];
-//   addItem: (item: Omit<Item, "id">) => void;
-// }
-
-// export const useItemStore = create<ItemStore>((set) => ({
-//   items: [],
-//   addItem: (item) =>
-//     set((state) => ({
-//       items: [...state.items, { id: state.items.length + 1, ...item }],
-//     })),
-// }));
 
 
 
